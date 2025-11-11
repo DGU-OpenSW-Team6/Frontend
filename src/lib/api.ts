a5f5ebf97d0e30db700730ed4a3146ce332c2d5f
MinHyuk-Park <mhsy10310@naver.com>
2025-11-11 20:34:10 +0900

    mixed content

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://52.78.81.44:8000',
+  baseURL: '/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
